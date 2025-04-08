0728d76213557679e6c03b7264f241c93dc27f7e
samuel daniele fernandez <samueldaniele70@gmail.com>
2025-04-08 12:03:51 +0200

    *version 2.0

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Estamos haciendo control de versiones con GIT");
+		System.out.println("Estamos haciendo control de versiones 2.0 con GIT");
 	}
 
 }
